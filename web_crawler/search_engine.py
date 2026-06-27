6f218d1afb40749ad9b98a61e1d9c2cae700deb2
adityaManjare <adityamanjare747@gmail.com>
2026-06-27 22:55:46 +0530

    Boolean Search for N tokens and Phrase Search for N tokens

--- web_crawler/search_engine.py
+++ web_crawler/search_engine.py
@@ -26,7 +26,7 @@ def read_postings(term : str , index ):
 
 
 
-def bool_and(a ,b ): # here i a and b are listsb
+def bool_and(a ,b ): # here i a and b are lists 
     ans = []
     i = j = 0
     while i < len(a) and j < len(b):
@@ -83,23 +83,134 @@ def bool_not(a , b):
 
 
 
-def search(query:str):
+def bool_search(query:str):
     tokens = query.split()
     if not tokens:
         return []
-    docs = []
-    if len(tokens) == 1:
-        docs = [x.doc_id for x in read_postings(query,index)]
-    else:
-        a = [x.doc_id for x in read_postings(tokens[0],index)]
-        b = [x.doc_id for x in read_postings(tokens[2],index)]
-        if tokens[1] == "AND":
-            docs = bool_and(a,b)
-        elif tokens[1] == "OR":
-            docs = bool_or(a,b)
+    precedence = {
+        "NOT" : 2,
+        "AND" : 1,
+        "OR" : 0
+    }
+    oprtr = []
+    oprnd = []
+    for token in tokens:
+        if token in precedence :
+            while oprtr and precedence[oprtr[-1]] >= precedence[token]:
+                b = oprnd.pop()
+                a = oprnd.pop() 
+                op = oprtr.pop()
+                if op == "AND":
+                    c = bool_and(a,b)
+                elif op == "OR":
+                    c = bool_or(a,b)
+                else:
+                    c = bool_not(a,b)
+                oprnd.append(c)
+            oprtr.append(token)
         else:
-            docs = bool_not(a,b)
-    return docs
+            a = [x.doc_id for x in read_postings(token,index)]
+            oprnd.append(a)
+    while oprtr:
+        b = oprnd.pop()
+        a = oprnd.pop()
+        op = oprtr.pop()
+        if op == "AND":
+            c = bool_and(a,b)
+        elif op == "OR":
+            c = bool_or(a,b)
+        else:
+            c = bool_not(a,b)
+        oprnd.append(c)
+    return oprnd.pop()
 
-query = "dress" 
-print(search(query))
+
+
+def merge_positions(pos1, pos2):
+    i = j = 0
+    matched = []
+
+    while i < len(pos1) and j < len(pos2):
+
+        if pos2[j] == pos1[i] + 1:
+            matched.append(pos2[j])
+            i += 1
+            j += 1
+
+        elif pos1[i] + 1 < pos2[j]:
+            i += 1
+
+        else:
+            j += 1
+
+    return matched
+
+
+def phrase_search(query):
+    words = query.split()
+    if not words:
+        return []
+
+    postings = [read_postings(word, index) for word in words]
+
+    #yup similar to how we do list initialization 
+    current = {
+        p.doc_id: p.positions
+        for p in postings[0]
+    }
+
+    for k in range(1, len(words)):
+        nxt = {
+            p.doc_id: p.positions
+            for p in postings[k]
+        }
+        new_current = {}
+        for doc in current:
+
+            if doc not in nxt:
+                continue
+            matched = merge_positions(current[doc], nxt[doc])
+            if matched:
+                new_current[doc] = matched
+
+        current = new_current
+        if not current:
+            return []
+
+    return list(current.keys())
+
+
+
+
+    # if len(tokens) == 1:
+    #     docs = [x.doc_id for x in read_postings(query,index)]
+    # else:
+
+
+
+    #     a = [x.doc_id for x in read_postings(tokens[0],index)]
+    #     b = [x.doc_id for x in read_postings(tokens[2],index)]
+    #     if tokens[1] == "AND":
+    #         docs = bool_and(a,b)
+    #     elif tokens[1] == "OR":
+    #         docs = bool_or(a,b)
+    #     else:
+    #         docs = bool_not(a,b)
+    # return docs
+
+
+
+
+
+
+
+# print(search("dream NOT butthol OR hello AND boy OR girl NOT hello"))
+# print(search("dream NOT butthol OR hello AND boy OR girl"))
+# print(search("dream NOT butthol OR hello AND boy"))
+# print(search("dream NOT butthol OR hello"))
+# print(search("dream NOT butthol "))
+# print(search("dream "))
+# print(search("boy"))
+# print(search("girl"))
+# print(search("boy AND girl NOT dream"))
+print(phrase_search("love you"))
